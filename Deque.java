ccf72bccffa75a76947daa432f3dd1e4d9f8bf03
NikolaA22 <32622417+NikolaA22@users.noreply.github.com>
2017-12-03 20:03:13 +0100

    first commit

---
+++ Deque.java
@@ -0,0 +1,56 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.lang.Iterable;
+
+
+public class Deque<Item> implements Iterable<Item> {
+
+    public Deque() {
+
+    }
+
+    public boolean isEmpty(){
+
+        return false;
+
+    }
+
+    public int size(){
+
+        return 0;
+    }
+
+    public void addFirst(Item item){
+
+
+
+    }
+
+    public void addLast(Item item){
+
+
+
+    }
+
+    public Item removeFirst(){
+
+       return null;
+
+    }
+
+    public Item removeLast(){
+
+        return null;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return null;
+    }
+
+    public static void main(String[] args){
+
+
+
+    }
+}
